e164be73591fc3f028711e8e58d5efb6dc69728d
Miguel González Pérez <me@miguelos.me>
2014-02-24 13:34:20 +0000

    Added bash to script

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 git pull
-rsync -v --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
+rsync --no-perms bash ~
