e3683567af52ddaa1bc7885c8f593a72468629e4
Miguel González Pérez <me@miguelos.me>
2014-02-26 16:56:25 +0000

    Updated dotfiles script

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
-#git pull
-rsync -av --no-perms bash/ ~
+
+#Check if argument is passed
+if [ $# -lt 1 ]
+then
+        echo "Usage ./dotfiles.sh [pull|push]"
+        exit
+fi
+
+case "$1" in
+
+'push')
+  echo "pushing"
+  rsync -av --exclude='[^.]*' --exclude='.Trash' --no-perms ~ bash/
+  ;;
+'pull')
+  echo "pull"
+  #git pull
+  rsync -av --no-perms bash/ ~
+  ;;
+esac
+
