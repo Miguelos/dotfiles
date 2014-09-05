ca1f240bc632770fc96af7247d572f683144643d
Miguel <me@miguelos.me>
2014-09-05 10:57:41 +0100

    Update list aliases :shell:

--- bash/.bash_aliases.d/general-aliases.sh
+++ bash/.bash_aliases.d/general-aliases.sh
@@ -1,9 +1,9 @@
 ## some more ls aliases
 # List aliases
-alias l='ls -cCF'
-alias ll='ls -l'
-alias la='ls -A'
-alias lg='ls -cCF  | grep '
+alias l='ls -cCFh'
+alias ll='ls -lh'
+alias la='ls -Ah'
+alias lg='ls -cCFh | grep '
 
 ##Compass watch
 alias cow='compass watch -e development --trace'
